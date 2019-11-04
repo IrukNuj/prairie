d2daadc24c4ab6624c24ce5c3ab0acf70e2c8652
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2019-11-05 03:59:24 +0900

    add: Runner task

--- lib/prairie.rb
+++ lib/prairie.rb
@@ -1,6 +1,11 @@
 require "prairie/version"
 
 module Prairie
-  class Error < StandardError; end
-  # Your code goes here...
+  # class Error < StandardError; end
+
+  class Runner
+    def self.run
+      Dir.mkdir(".github")
+    end
+  end
 end
